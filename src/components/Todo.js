d98804c2e4b8d5c6ab6be7f058108b27b9c65696
raqueleucaria <raqueleucaria@gmail.com>
2022-06-17 00:56:26 -0300

    estrutura

---
+++ src/components/Todo.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+import TodoForm from './TodoForm';
+
+
+
+function Todo({todos, completeTodo, removeTodo, updateTodo}) {
+    const [edit, setEdit] = useState({
+        id: null,
+        value: ''
+    });
+
+    const submitUpdate = value => {
+        updateTodo(edit.id, value)
+        setEdit({
+            id: null,
+            value: ''
+        })
+    };
+
+    if(edit.id) {
+        return <TodoForm edit={edit} onSubmit={submitUpdate}/>;
+    }
+
+    return todos.map((todo, index) => (
+        <div 
+            className={todo.isComplete ? 'todo-row complete': 
+            'todo-row'} key={index}
+        >
+            <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+                {todo.text}
+            </div>
+            <div className='icons'>
+                <button className='removeButton' onClick={() => removeTodo(todo.id)}>x</button>
+                <button 
+                    className='editButton' 
+                    onClick={() => setEdit({id: todo.id, value: todo.text})}
+                >**
+                </button>
+
+            </div>
+        </div> 
+    ))
+}
+
+export default Todo
---
+++ src/components/TodoForm.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+
+
+function TodoForm(props) {
+   const [input, setInput] = useState('');  
+
+   const handChange = e => {
+        setInput(e.target.value);
+   }
+   
+   const handleSubmit = e => {
+        e.preventDefault();
+    
+        props.onSubmit({
+            id: Math.floor(Math.random() * 1000),
+            text: input
+        });
+
+        setInput('')
+    }
+
+    
+    
+
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+        <input 
+            type="text" 
+            placeholder="Add a todo" 
+            value={input} 
+            name='text' 
+            className="todo-input"
+            onChange={handChange}
+        />
+        <button className="todo-button">Add todo</button>
+    </form>
+  )
+}
+
+export default TodoForm
